1143dcfa597d017e50246a8ab566685708518bdb
johncburnett <jburnett@oberlin.edu>
2015-09-26 23:10:36 -0400

    Debugged FFT_Analyzer, implemented modal.py

--- src/FFT_Analyzer.py
+++ src/FFT_Analyzer.py
@@ -1,11 +1,12 @@
 from scipy.fftpack import fft, ifft
 from numpy import absolute
+from copy import deepcopy
 from WAV_Reader import WAV_Reader
 
 class FFT_Analyzer:
 
 	def __init__(self, wav_file, n_points=256):
-		self.wav_name = ''
+		self.wav_name = wav_file
 		self.wav_data = []
 		self.wav_sample_rate = 44100
 		self.fft_data = []
@@ -14,24 +15,24 @@ class FFT_Analyzer:
 
 
 	def extract_samples(self):
-		wav_extract = WAV_Reader('ex.wav')
-		wav_extract.extract_samples('ex.wav')
+		wav_extract = WAV_Reader(self.wav_name)
 		wav_extract.toMono()
 		self.wav_data = wav_extract.data
 		self.wav_sample_rate = wav_extract.sampleRate
 
 
 	def fft_analysis(self):
-		self.fft_data = fft(self.wav_data, self.fft_n_points)
+		self.fft_data = list(fft(self.wav_data, self.fft_n_points))
+
 
 
 	def generate_bins(self):
 		magnitudes = fft_to_magnitude(self.fft_data)
-		freq_res = self.wav_sample_rate / fft_n_points
+		freq_res = self.wav_sample_rate / self.fft_n_points
 		num_bins = self.fft_n_points / 2
 
-		for i in range(1,num_bins):
-			self.bins[i].append([freq_res*i, magnitudes[i]])
+		for i in range(1,len(magnitudes)):
+			self.bins.append([freq_res*i, magnitudes[i]])
 
 
 	def perform_analysis(self):
@@ -43,7 +44,8 @@ class FFT_Analyzer:
 	#---------------------------------------------------------------------
 	#_Utilities
 
-	def fft_to_magnitude(fft_array):
-		for x in fft_array:
-			x = numpy,absolute(x)
-		return fft_array
+def fft_to_magnitude(fft_array):
+	fft_array = deepcopy(fft_array)
+	for i in range(len(fft_array)):
+		fft_array[i] = absolute(fft_array[i])
+	return fft_array
--- src/WAV_Reader.py
+++ src/WAV_Reader.py
@@ -23,14 +23,14 @@ class WAV_Reader:
                 temp += c
             temp = temp/self.channels
             mono_data.append(temp)
-        mono_data = norm(mono_data)
+        mono_data = self.norm(mono_data)
         self.data = mono_data
         
     def norm(self, data):
         maxSample = 0
         scalar = 0
         for s in data:
-            if abs(s) > maxSamle:
+            if abs(s) > maxSample:
                 maxSample = s
         scalar = 1/maxSample
         for s in data:
