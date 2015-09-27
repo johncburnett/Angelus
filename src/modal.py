569ff5c1dadc7f5761bb207a1b10be2c2ceee5ab
wmejohnson <willejohns@gmail.com>
2015-09-27 01:49:04 -0400

    added toRObU method to modal.py, probably not the best place for it, but it'll do for now. need to get the duration of each partial so that the damping can be computed.

--- src/modal.py
+++ src/modal.py
@@ -1,8 +1,26 @@
 from FFT_Analyzer import FFT_Analyzer
 
 def main():
-	analysis = FFT_Analyzer('test.wav')
-	analysis.perform_analysis()
-	print analysis.bins
+    analysis = FFT_Analyzer('test.wav')
+    analysis.perform_analysis()
+    print analysis.bins
+    toRObU(analysis)
+    
+def toRObU(analysis, outfile="test.ro"):
+    fileout = open(outfile, "w")
+    fileout.write("nactive_freq:\n")
+    fileout.write(str(len(analysis.bins))+"\n")
+    fileout.write("frequencies:\n")
+    for bin in analysis.bins:
+        fileout.write(str(bin[0])+"\n")
+    fileout.write("dampings:\n")
+    for bin in analysis.bins:
+        # don't have this yet
+        fileout.write("error\n")
+    fileout.write("amplitudes[point][freq]:\n")
+    for bin in analysis.bins:
+        fileout.write(str(bin[1])+"\n")
+    fileout.write("END\n")
+        
 
 main()
