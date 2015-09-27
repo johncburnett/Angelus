e413fffba68965b08d87d5ad976f2415ccda21d0
wmejohnson <willejohns@gmail.com>
2015-09-26 23:55:26 -0400

    implemented n_loudest_partials

--- src/FFT_Analyzer.py
+++ src/FFT_Analyzer.py
@@ -50,11 +50,26 @@ class FFT_Analyzer:
 		 	self.bins[1] = amplitudes.append(i)
 
 
+    def n_loudest_partials(self, n):
+        amplitudes = []
+        amp_dict = {}
+        for bin in self.bins:
+            amplitudes.append(bin[1])
+            amp_dict[bin[1]] = bin[0]
+        amplitudes.sort()
+        amplitudes = amplitudes[-n:]
+        new_bins = []
+        for amp in amplitudes:
+            new_bins.append([amp_dict[amp], amp])
+        self.bins = new_bins
+        
+            
 	def perform_analysis(self):
 		self.extract_samples()
 		self.fft_analysis()
 		self.generate_bins()
 		self.normalize_amplitudes()
+        self.n_loudest_partials(10)
 
 
 	#---------------------------------------------------------------------
---
+++ src/partialTracking.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+class Partial_Tracker():
+    
+    def __init__ (self):
+        self.maxdeviation
+        self.maxpartialgap
+        self.minlength
+        self.minamp
+    
+    def track(self, f):
+        
+        
